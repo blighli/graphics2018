263ecdadc3d1775774f79295e95097f69b4fe770
Student-HXJ <Student-HXJ@github.com>
2019-01-01 12:08:40 +0800

    Project02

---
+++ 21860432+黄小洁/Project02/cmake-build-debug/CMakeFiles/Project02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project02.dir/glad.c.o"
+  "CMakeFiles/Project02.dir/main.cpp.o"
+  "CMakeFiles/Project02.dir/stb_image.cpp.o"
+  "Project02.pdb"
+  "Project02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Project02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
