bea7d9756f28e9200bc148ec96000c5ab5538ecb
yhyao <39327721+yhyao@users.noreply.github.com>
2019-01-05 20:40:36 +0800

    Add files via upload

---
+++ 21851090姚宇辉/Project03/Pic.py
@@ -0,0 +1,26 @@
+
+"""顶点类"""
+class Point3:
+    def __init__(self,X,Y,Z):
+        self.X=X
+        self.Y=Y
+        self.Z=Z
+"""纹理类"""
+class WenLi:
+    def __init__(self,TU,TV):
+        self.TU=TU
+        self.TV=TV
+"""法向量类"""
+class FaVector:
+    def __init__(self,NX,NY,NZ):
+        self.NX=NX
+        self.NY=NY
+        self.NZ=NZ
+
+class Mian:
+    def __init__(self,V,T,N):
+        self.V=V
+        self.T=T
+        self.N=N
+
+
