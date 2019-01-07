bf53bdde962377eced1e8cfb0668af6bbd4d509a
kimvra <787178256@qq.com>
2019-01-07 08:29:09 +0800

    project03

---
+++ 21851483黄鑫鑫/Project03/ObjLoader.hpp
@@ -0,0 +1,29 @@
+//
+//  ObjLoader.hpp
+//  StudyOpengl
+//
+//  Created by 贺晨韬 on 2019/1/6.
+//  Copyright © 2019 贺晨韬. All rights reserved.
+//
+
+#ifndef ObjLoader_hpp
+#define ObjLoader_hpp
+
+#include <stdio.h>
+#include "Dependencies/glew/glew.h"
+#include "Dependencies/freeglut/freeglut.h"
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+class ObjLoader{
+public:
+    ObjLoader(string filename);//构造函数
+    void Draw();//绘制函数
+private:
+    vector<vector<GLfloat>>vSets;//存放顶点(x,y,z)坐标
+    vector<vector<GLint>>fSets;//存放面的三个顶点索引
+};
+
+#endif /* ObjLoader_hpp */
