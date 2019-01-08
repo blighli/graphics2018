51ae78408216bcdb5fef2c729550ce56c3ec9a82
Marveliu <897920245@qq.com>
2019-01-08 11:03:21 +0800

    add:homework3

---
+++ 21851164刘尚楠/homework3/homework3/ObjLoader.hpp
@@ -0,0 +1,20 @@
+//
+//  ObjLoader.hpp
+//
+//  Created by Marveliu on 2019/1/3.
+//  Copyright © 2019 Marveliu. All rights reserved.
+//
+
+#include "glew.h"
+#include "glut/glut.h"
+#include <vector>
+#include <string>
+using namespace std;
+class ObjLoader{
+public:
+    ObjLoader(string filename);//构造函数
+    void Draw();//绘制函数
+private:
+    vector<vector<GLfloat>>vSets;//存放顶点(x,y,z)坐标
+    vector<vector<GLint>>fSets;//存放面的三个顶点索引
+};
